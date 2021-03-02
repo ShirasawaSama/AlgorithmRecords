6662c94f381473e4d7bcbfdafc7cb3cc5fbd33e0
Shirasawa <764798966@qq.com>
2021-03-02 22:27:38 +0800

    Update Matrix.java

--- templates/Matrix.java
+++ templates/Matrix.java
@@ -1,12 +1,25 @@
 class Matrix {
-    public static void mul(long[][] a, long[][] b) {
-        for (int i = 0, len = b.length; i < len; i++) {
-            for (int j = 0; j < len; j++) {
-                for (int k = 0; k < len; k++) {
-                    a[i][j] = (a[i][j] + a[i][k] * a[k][j]);
-                }
-                a[i][j] %= 10000007;
+    public static long[][] pow(long[][] x, long y) {
+        int len = x.length;
+        long[][] ans = new long[len][len];
+        for (int i = 0; i < len; i++) ans[i][i] = 1;
+        while (y != 0) {
+            if((y & 1) == 1) ans = mul(ans, x);
+            x = mul(x,x);
+            y >>= 1;
+        }
+        return ans;
+    }
+
+    public static long[][] mul(long[][] a, long[][] b) {
+        int len1 = a.length, len2 = b[0].length, len3 = b.length;
+        long[][] ans = new long[len1][len2];
+        for (int i = 0; i < len1; i++) {
+            for (int j = 0; j < len2; j++) {
+                for (int k = 0; k < len3; k++) ans[i][j] = ans[i][j] + a[i][k] * b[k][j];
+                ans[i][j] %= 10000007;
             }
         }
+        return ans;
     }
 }
