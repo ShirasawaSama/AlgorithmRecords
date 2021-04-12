fbcf2acc1d9b1a1e1803d33c3e3a13fa1b7452ef
Shirasawa <764798966@qq.com>
2021-04-13 00:09:25 +0800

    Update Prime.java

--- templates/Prime.java
+++ templates/Prime.java
@@ -12,4 +12,28 @@ class Prime {
             if (prime[i]) for (int j = i * i; j <= n; j += i) prime[j] = false;
         }
     }
+    
+    private static boolean isPrime2(long x) {
+        if (x < 3) return x == 2;
+        if (x % 2 == 0) return false;
+        long d = x - 1, r = 0;
+        while (d % 2 == 0) {
+            d /= 2;
+            ++r;
+        }
+        for (long a : A) {
+            long v = fastPower(a, d, x);
+            if (v <= 1 || v == x - 1) continue;
+            for (int i = 0; i < r; ++i) {
+                v = v * v % x;
+                if (v == x - 1 && i != r - 1) {
+                    v = 1;
+                    break;
+                }
+                if (v == 1) return false;
+            }
+            if (v != 1) return false;
+        }
+        return true;
+    }
 }
